41cdf648cdeb5eea087d19a18a423f5b8e9a3841
Gérald Döserich <github@doeseri.ch>
2025-03-03 13:11:31 +0100

    Fixed not being able to rate 0

--- app/forms/school/RateSchoolForm.py
+++ app/forms/school/RateSchoolForm.py
@@ -3,5 +3,5 @@ from wtforms import DecimalRangeField, TextAreaField
 from wtforms.validators import DataRequired, NumberRange
 
 class RateSchoolForm(FlaskForm):
-    rating = DecimalRangeField('Rate', validators=[DataRequired(), NumberRange(min=0, max=5)], render_kw={'step': '0.5', 'min': '0', 'max': '5'})
+    rating = DecimalRangeField('Rate', validators=[NumberRange(min=0, max=5)], render_kw={'step': '0.5', 'min': '0', 'max': '5'})
     comment = TextAreaField('Comment', validators=[DataRequired()])
